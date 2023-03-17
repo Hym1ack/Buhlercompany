35310f9139f5971b1450ced25e6fe557ec5b6c0b
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-17 15:00:58 +0300

    подключение vite, tailwind, eslint, prettier, typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import viteImagemin from 'vite-plugin-imagemin';
+import svgr from 'vite-plugin-svgr';
+import tailwindcss from 'tailwindcss';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    tailwindcss(),
+    svgr(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
+});
