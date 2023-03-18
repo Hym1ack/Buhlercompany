71fd6911c62ff5e438611133ca6c8c836167864c
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-18 23:25:22 +0300

    types для png svg

---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+declare module '*.png' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
