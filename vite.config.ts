264b2fdf50bc75a75606a971b15cf9088cc9ce8b
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-22 13:31:34 +0300

    раскидал js, images по отдельным папкам в prod

--- vite.config.ts
+++ vite.config.ts
@@ -40,4 +40,24 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 });
